b31c7d62d1f6fda5079e117aa2ad1cf817d77401
Joey <987102818@qq.com>
2019-03-03 20:28:22 -0800

    update sicp3.rkt

--- sicp3.rkt
+++ sicp3.rkt
@@ -4,3 +4,27 @@
 (define A (make-accumulator 5))
 (A 10)
 (A 10)
+
+(define random-init 100)
+(define (rand-update x)
+  (+ x 1))
+
+(define rand_user
+  (let ((x random-init))
+    (lambda ()
+      (set! x (rand-update x))
+      x)))
+
+(rand_user)
+(rand_user)
+
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((tmp (cdr x)))
+          (set-cdr! x y)
+          (loop tmp x))))
+  (loop x '()))
+(define v (list 'a 'b 'c 'd))
+(define w (mystery v))
